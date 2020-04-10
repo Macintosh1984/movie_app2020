90210da9528498326775a90cf54f1e04f5b6c981
Macintosh1984 <bachyeonsu@gmail.com>
2020-04-10 14:05:34 +0900

    Updates

---
+++ precache-manifest.9be456b9c0439c54073a2d119ce13b19.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3ef73e6461951fe1aa8226b1f7aee12",
+    "url": "/movie_app2020/index.html"
+  },
+  {
+    "revision": "859fc86722285fd38266",
+    "url": "/movie_app2020/static/css/main.f087e558.chunk.css"
+  },
+  {
+    "revision": "31a3afda46776f5d4df4",
+    "url": "/movie_app2020/static/js/2.39a23c44.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app2020/static/js/2.39a23c44.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "859fc86722285fd38266",
+    "url": "/movie_app2020/static/js/main.a8958956.chunk.js"
+  },
+  {
+    "revision": "c6af71ba2235e8f8bbb1",
+    "url": "/movie_app2020/static/js/runtime-main.3ed47806.js"
+  }
+]);
